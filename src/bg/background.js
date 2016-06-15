55d3ff970a979b4c11c71c97610444fe749b43e6
cassidoo <cassidy@clarifai.com>
2016-06-15 16:05:42 -0400

    Add Clarifai and TripAdvisor url function

--- src/bg/background.js
+++ src/bg/background.js
@@ -5,8 +5,18 @@ chrome.contextMenus.create({
 });
 
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
-  console.log(info.linkUrl);
-  chrome.tabs.create({
-    "url" : info.linkUrl
-  });
+  generateLink(info.linkUrl);
 });
+
+function generateLink(imgurl) {
+  Clarifai.getTagsByUrl(imgurl, {
+    'model': 'travel-v0.1'
+  }).then(function(r){
+    var tag = r.results[0].result.tag.classes[0].replace(/ /g,"+");
+    chrome.tabs.create({
+      "url" : "https://www.tripadvisor.com/Search?q=" + tag
+    });
+  }, function(err) {
+    console.log(err);
+  });
+}
