e580b44f2bba727de05e85f3437823f5d21a810e
cassidoo <cassidy@clarifai.com>
2016-06-15 11:14:07 -0400

    Initial context menu image capture

---
+++ src/bg/background.js
@@ -0,0 +1,26 @@
+// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
+
+// var settings = new Store("settings", {
+//     "sample_setting": "This is how you use Store.js to remember values"
+// });
+
+
+//example of using a message handler from the inject scripts
+/*chrome.extension.onMessage.addListener(
+  function(request, sender, sendResponse) {
+  	chrome.pageAction.show(sender.tab.id);
+    sendResponse();
+  });
+*/
+chrome.contextMenus.create({
+  "title": "Tripadifai",
+  "contexts": ["image"],
+  "onclick": clickHandler
+});
+
+var clickHandler = function(e) {
+  var imgUrl = encodeURI(e.srcUrl);
+  chrome.tabs.create({
+    "url" : imgUrl
+  });
+};
